80c1144bb964198a28973a8857983baee831777e
kubota20 <doc0000doc77@gmail.com>
2024-05-23 23:56:53 +0900

    テスト用です。

---
+++ my-app/src/data/testData.ts
@@ -0,0 +1,10 @@
+export const testData = [
+  {
+    name: "Project Name",
+    summary: "This is a brief summary of Project One.",
+    technologies: ["React", "Next.js", "CSS"],
+    github: "https://github.com/username/project-one",
+    demo: "https://project-one-demo.com",
+    image: "/images/project-one.png",
+  },
+];
---
+++ my-app/src/types/test-data.ts
@@ -0,0 +1,8 @@
+export type testDataProps = {
+  name: string;
+  summary: string;
+  technologies: string[];
+  github: string;
+  demo: string;
+  image: string;
+};
