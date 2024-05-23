f979d430a845a3932dab7838ac6708d5e8e2ad39
kubota20 <doc0000doc77@gmail.com>
2024-05-23 15:15:17 +0900

    初期HomePage

--- my-app/src/app/(routes)/page.tsx
+++ my-app/src/app/(routes)/page.tsx
@@ -1,7 +1,8 @@
+import Container from "@/components/ui/container";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <Container>HomePage</Container>;
 };
 
 export default HomePage;
