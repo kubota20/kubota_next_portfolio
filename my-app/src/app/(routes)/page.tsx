eef65ef09db021dcab60e373b20f265c0ccd975a
kubota20 <doc0000doc77@gmail.com>
2024-05-24 00:22:29 +0900

    ProjectList追加

--- my-app/src/app/(routes)/page.tsx
+++ my-app/src/app/(routes)/page.tsx
@@ -1,8 +1,20 @@
-import Container from "@/components/ui/container";
 import React from "react";
 
+import ProjectList from "@/components/project-list";
+import Container from "@/components/ui/container";
+
 const HomePage = () => {
-  return <Container>HomePage</Container>;
+  return (
+    <main>
+      <Container>
+        <div>{/* 画像か動画あればあとで入れる */}</div>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProjectList title="レイアウト" />
+        </div>
+      </Container>
+      ;
+    </main>
+  );
 };
 
 export default HomePage;
