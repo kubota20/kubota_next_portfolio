9678c677e4af1e136ea1cb7a47b478c8b2750cea
kubota20 <doc0000doc77@gmail.com>
2024-05-24 15:56:17 +0900

    ProjectsPageにProjectList追加

--- my-app/src/app/(routes)/projects/page.tsx
+++ my-app/src/app/(routes)/projects/page.tsx
@@ -1,9 +1,17 @@
 // プロジェクト全体のページ
 
+import ProjectList from "@/components/project-list";
+import Container from "@/components/ui/container";
 import React from "react";
 
 const ProjectsPage = () => {
-  return <div>ProjectsPage</div>;
+  return (
+    <Container>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 mb-10">
+        <ProjectList title="レイアウト" />
+      </div>
+    </Container>
+  );
 };
 
 export default ProjectsPage;
