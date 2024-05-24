640059a17b393fbc5b1a57a9f47c52c22c9974ef
kubota20 <doc0000doc77@gmail.com>
2024-05-24 15:40:09 +0900

    BlogList追加

--- my-app/src/app/(routes)/page.tsx
+++ my-app/src/app/(routes)/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import ProjectList from "@/components/project-list";
 import Container from "@/components/ui/container";
+import { Separator } from "@/components/ui/separator";
+import BlogList from "@/components/blog-list";
 
 const HomePage = () => {
   return (
@@ -10,9 +12,12 @@ const HomePage = () => {
         <div>{/* 画像か動画あればあとで入れる */}</div>
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProjectList title="レイアウト" />
+
+          <Separator />
+
+          <BlogList title="ブログ" />
         </div>
       </Container>
-      ;
     </main>
   );
 };
