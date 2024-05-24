8d08fa9555ff61718b8dbc8916cfc905c81b1ad4
kubota20 <doc0000doc77@gmail.com>
2024-05-24 15:55:57 +0900

    BlogsPageにBlogList追加

--- my-app/src/app/(routes)/blogs/page.tsx
+++ my-app/src/app/(routes)/blogs/page.tsx
@@ -1,7 +1,16 @@
 // ブログ全体の記事
 
+import BlogList from "@/components/blog-list";
+import Container from "@/components/ui/container";
+
 const BlogsPage = () => {
-  return <div>BlogsPage</div>;
+  return (
+    <Container>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 mb-10">
+        <BlogList title="レイアウト" />
+      </div>
+    </Container>
+  );
 };
 
 export default BlogsPage;
