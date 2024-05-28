9d6827039e93b8d414bfc9864b23adaa005be195
kubota20 <doc0000doc77@gmail.com>
2024-05-28 12:43:23 +0900

    useModilModalを使ってModilModalが出るようにする

--- my-app/src/components/ui/project-card.tsx
+++ my-app/src/components/ui/project-card.tsx
@@ -1,11 +1,32 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { Smartphone, TabletSmartphone } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import Infos from "@/components/ui/infos";
 
+import { useModilModal } from "@/hooks/use-mobil-modal";
+
 import { testData } from "@/data/testData";
 
 const ProjectCard = () => {
+  const modilModal = useModilModal();
+  const testData = useModilModal((state) => state.data);
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+
+  const onLink = () => {
+    setOpen(false);
+    router.push("/");
+  };
+
+  const onModilModal = () => {
+    setOpen(false);
+    modilModal.onOpen(testData);
+  };
+
   return (
     <div className="bg-white group rounded-xl border p-3 space-y-4">
       <div className="aspect-square rounded-xl bg-gray-100 relative">
@@ -14,18 +35,11 @@ const ProjectCard = () => {
         {/* レスポンシブボタン */}
         <div className="opacity-0 bg-black absolute bottom-0 right-0 border rounded-xl sm:opacity-70">
           <div className="flex items-center justify-center">
-            {/* スマホボタン */}
-            <Button
-              size="icon"
-              className="text-gray-600 cursor-pointe hover:text-white"
-            >
-              <Smartphone size={20} />
-            </Button>
-            {/* タブレットボタン */}
             <Button size="icon">
               <TabletSmartphone
                 size={20}
                 className="text-gray-600 cursor-pointe hover:text-white"
+                onClick={onModilModal}
               />
             </Button>
           </div>
@@ -33,9 +47,7 @@ const ProjectCard = () => {
       </div>
       {/* 名前や使用技術、ページリンク */}
 
-      {testData.map((item) => (
-        <Infos key={item.name} item={item} />
-      ))}
+      <Infos key={testData.name} item={testData} />
     </div>
   );
 };
