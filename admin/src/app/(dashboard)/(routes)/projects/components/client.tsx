3b7bcd4f52c35ef6ba3d40c916676d2a02f83853
kubota20 <doc0000doc77@gmail.com>
2024-06-06 19:04:13 +0900

    ProjectClient　作成

--- admin/src/app/(dashboard)/(routes)/projects/components/client.tsx
+++ admin/src/app/(dashboard)/(routes)/projects/components/client.tsx
@@ -2,10 +2,33 @@
 
 import { DataTable } from "@/components/ui/data-teble";
 import { ProjectColumnProps, columns } from "./columns";
-export const ProjectClient = () => {
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+
+interface ProductClientProps {
+  data: ProjectColumnProps[];
+}
+
+export const ProjectClient = ({ data }: ProductClientProps) => {
+  const router = useRouter();
   return (
-    <div>
-      <DataTable searchKey="name" columns={columns} data={[]} />
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Project (${data.length})`}
+          description="Projectsページにようこそ！"
+        />
+        <Button onClick={() => router.push(`/projects/new`)}>
+          <Plus className="mr-2 h-4 w-4" /> 追加
+        </Button>
+      </div>
+
+      <Separator />
+
+      <DataTable searchKey="name" columns={columns} data={data} />
+    </>
   );
 };
