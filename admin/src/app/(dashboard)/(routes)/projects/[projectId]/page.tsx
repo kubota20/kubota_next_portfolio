74ead5f366961bf17b62a77a56b7e8f9c181b8ad
kubota20 <doc0000doc77@gmail.com>
2024-06-06 19:00:11 +0900

    Project Pages作成

---
+++ admin/src/app/(dashboard)/(routes)/projects/[projectId]/page.tsx
@@ -0,0 +1,23 @@
+import { prisma } from "@/lib/prisma";
+import React from "react";
+import { ProjectForm } from "./components/project-form";
+
+const ProjectPage = async ({ params }: { params: { projectId: string } }) => {
+  const project = await prisma.project.findUnique({
+    where: {
+      id: params.projectId,
+    },
+  });
+
+  const categories = await prisma.category.findMany();
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProjectForm categories={categories} initiaData={project} />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectPage;
--- admin/src/app/(dashboard)/(routes)/projects/page.tsx
+++ admin/src/app/(dashboard)/(routes)/projects/page.tsx
@@ -1,12 +1,38 @@
+import { prisma } from "@/lib/prisma";
+
 import Container from "@/components/ui/container";
 
 import { ProjectClient } from "./components/client";
+import { ProjectColumnProps } from "./components/columns";
 
-const ProjectsPage = () => {
+import { format } from "date-fns";
+
+const ProjectsPage = async () => {
+  // findMany	複数件取得 条件に一致する全てのレコードを取得
+  const projects = await prisma.project.findMany({
+    include: {
+      category: true, // カテゴリーデータを含める
+    },
+  });
+
+  const formattedProjectsColumn: ProjectColumnProps[] = projects.map(
+    (item) => ({
+      id: item.id,
+      title: item.title,
+      summary: item.summary,
+      iframeSrc: item.iframeSrc,
+      link: item.link,
+      imageUrl: item.imageUrl,
+      category: item.category?.name,
+      release: item.release,
+      createdAt: format(item.createdAt, "yyyy年MM月dd日 HH:mm"),
+    })
+  );
+
   return (
-    <div className="my-12 flex flex-1">
+    <div className="pt-6 flex flex-1 p-8">
       <Container>
-        <ProjectClient />
+        <ProjectClient data={formattedProjectsColumn} />
       </Container>
     </div>
   );
