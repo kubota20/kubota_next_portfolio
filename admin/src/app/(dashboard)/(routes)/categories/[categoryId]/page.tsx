919a0550c1c6c4572615642fbb4f216f8df64a95
kubota20 <doc0000doc77@gmail.com>
2024-06-07 16:04:42 +0900

    CategoryPage　作成

---
+++ admin/src/app/(dashboard)/(routes)/categories/[categoryId]/page.tsx
@@ -0,0 +1,20 @@
+import { prisma } from "@/lib/prisma";
+import { CategoryForm } from "./components/category-form";
+
+const CategoryPage = async ({ params }: { params: { categoryId: string } }) => {
+  const category = await prisma.category.findUnique({
+    where: {
+      id: params.categoryId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryForm initiaData={category} />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
