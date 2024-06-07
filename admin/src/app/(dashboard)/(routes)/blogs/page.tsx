afdefc63023c033af40bbe35e327e8b67983b87e
kubota20 <doc0000doc77@gmail.com>
2024-06-07 17:24:10 +0900

    blogsPage作成

--- admin/src/app/(dashboard)/(routes)/blogs/page.tsx
+++ admin/src/app/(dashboard)/(routes)/blogs/page.tsx
@@ -1,14 +1,38 @@
-import Container from "@/components/ui/container";
-import React from "react";
+import { prisma } from "@/lib/prisma";
 
-const BlogsPage = () => {
+import Container from "@/components/ui/container";
+
+import { BlogClient } from "./components/client";
+import { BlogColumnProps } from "./components/columns";
+
+import { format } from "date-fns";
+
+const blogsPage = async () => {
+  // findMany	複数件取得 条件に一致する全てのレコードを取得
+  const blogs = await prisma.blog.findMany({
+    include: {
+      category: true, // カテゴリーデータを含める
+    },
+  });
+
+  const formattedblogsColumn: BlogColumnProps[] = blogs.map((item) => ({
+    id: item.id,
+    title: item.title,
+    summary: item.summary,
+    link: item.link,
+    imageUrl: item.imageUrl,
+    category: item.category?.name,
+    release: item.release,
+    createdAt: format(item.createdAt, "yyyy年MM月dd日 HH:mm"),
+  }));
+
   return (
-    <div className="my-12 flex flex-1">
+    <div className="pt-6 flex flex-1 p-8">
       <Container>
-        <div className="">item</div>
+        <BlogClient data={formattedblogsColumn} />
       </Container>
     </div>
   );
 };
 
-export default BlogsPage;
+export default blogsPage;
